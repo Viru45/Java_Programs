bb36dbe6c3770554895f24e99fa1a717bc410b35
Virendra <147747413+Viru45@users.noreply.github.com>
2024-09-13 15:26:34 +0530

    Add files via upload

---
+++ Program2.java
@@ -0,0 +1,30 @@
+import java.awt.*;  
+import java.awt.event.*;  
+
+class Program2 extends Frame implements ActionListener{  
+TextField tf;  
+
+Program2(){  
+  
+//create components  
+tf=new TextField();  
+tf.setBounds(60,50,170,20);  
+Button b=new Button("click me");  
+b.setBounds(100,120,80,30);  
+  
+ 
+b.addActionListener(this);
+
+add(b);add(tf);  
+setSize(300,300);  
+setLayout(null);  
+setVisible(true);  
+
+}  
+public void actionPerformed(ActionEvent e){  
+tf.setText("Welcome");  
+}  
+public static void main(String args[]){  
+new Program2();  
+}  
+}  
